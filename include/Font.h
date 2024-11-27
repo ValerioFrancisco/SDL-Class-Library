ae51852dac471e51bef72a781cfe555d42cb128c
ValerioFrancisco <93736573+ValerioFrancisco@users.noreply.github.com>
2024-11-27 21:07:09 +0000

    Added Font.h

---
+++ include/Font.h
@@ -0,0 +1,25 @@
+#ifndef FONT_H
+#define FONT_H
+#include <ErrorTracker.h>
+
+namespace lp {
+
+	class Font() {
+		public:
+			Font();
+			virtual ~Font();
+
+			virtual TTF_Font *GetFont()const;
+			virtual bool Error()const;
+			virtual const char* ErrorMsg()const;
+
+			void Create(const char *file, int size);
+			void Close();
+
+		private:
+			TTF_Font *font;
+			ErrorTracker error;
+	};
+
+} // namespace
+#endif
---
+++ src/Font.cpp
@@ -0,0 +1,36 @@
+#include <SDL.h>
+#include <SDL_ttf.h>
+#include <ErrorTracker.h>
+
+namespace lp {
+
+	Font::Font(): font(NULL), error(ErrorTracker()) {}
+
+	Font::~Font() { Close(); }
+
+	TTF_Font *Font::GetFont()const { return font; }
+
+	bool Font::Error()const { return error(); }
+
+	const char *Font::ErrorMsg()const { return error.Message(); }
+
+	void Font::Create(const char *file, int size) {
+		if(!error()) Close();
+		font = TTF_OpenFont(file, size);
+		if(font == NULL) {
+			error.Set(true, TTF_GetError());
+		}
+		else {
+			error.Set(false, "");
+		}
+	}
+
+	void Font::Close() {
+		if(!error || font != NULL) {
+			TTF_CloseFont(font);
+			font = NULL;
+			error.Set(true, "Not Initialized");
+		}
+	}
+
+} // namespace
